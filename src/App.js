7a14ae63f5f26b7938df2e5e1d4a723bfe4cf969
Sanyam Khurana <sanyam.khurana01@gmail.com>
2024-10-10 08:16:04 +0530

    fix: Update Router basename

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
   }, []);
 
   return (
-    <Router>
+    <Router basename="/til-site">
       <Routes>
         <Route path="/" element={<Posts posts={posts} />} />
         <Route path="/:path" element={<PostDetail posts={posts} />} />
